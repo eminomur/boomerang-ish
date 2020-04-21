dc5f7a778cef6ea8f0eb67fb51a4c1b173650155
Muhammed Emin ÖMÜR <m.eminomur@outlook.com>
2020-04-21 16:16:29 +0300

    Add fixed window size and icon, change window title

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
+// This app works like Boomerang application with some additional features
+
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSize>
+#include <QIcon>
+
+#include "app_config.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setFixedSize(QSize(APP_DISPLAY_WIDTH, APP_DISPLAY_HEIGHT));
+    w.setWindowIcon(QIcon(":/icons/boomerang.svg"));
+    w.setWindowTitle("Boomerang(ish)");
     w.show();
     return a.exec();
 }
