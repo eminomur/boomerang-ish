4e55698dcde0caeffd1d4f12eb2e99e80185bf49
Muhammed Emin ÖMÜR <m.eminomur@outlook.com>
2020-04-21 16:11:03 +0300

    Add application constants

--- app_config.h
+++ app_config.h
@@ -1,4 +1,15 @@
 #ifndef APP_CONFIG_H
 #define APP_CONFIG_H
 
+const int APP_DISPLAY_WIDTH = 500;
+const int APP_DISPLAY_HEIGHT = 600;
+
+const int FRAME_WIDTH = 480;
+const int FRAME_HEIGHT = 480;
+
+const int MILLISECONDS_IN_ONE_SECOND = 1000;
+
+const int FRAME_BUFFER_SIZE = 25;
+const int FRAME_PER_SECOND = 50;
+
 #endif // APP_CONFIG_H
